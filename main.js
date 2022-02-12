072bcd50fd488e0f6e83ba54ab53630390dc3783
vigneshram2011 <78680381+vigneshram2011@users.noreply.github.com>
2022-02-12 15:53:32 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,61 @@
+status = "";
+objects = [];
+
+function preload() {}
+
+function setup() {
+    canvas = createCanvas(480, 380);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.size(380, 380);
+    video.hide();
+}
+
+function start() {
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects!";
+    object_name = document.getElementById("object_name").value;
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!")
+    status = true;
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw() {
+    image(video, 0, 0, 480, 380);
+    if (status != "") {
+        objectDetector.detect(video, gotResult);
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + "" + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+            if (objects[i].label == object_name) {
+                video.stop();
+                objectDetector.detect(gotResult);
+                document.getElementById("object_status").innerHTML = object_name + " found";
+
+                synth = window.speechSynthesis;
+                utterThis = new SpeechSynthesisUtterance(object_name + " found");
+                synth.speak(utterThis);
+            } else {
+                document.getElementById("object_status").innerHTML = object_name + " not found";
+            }
+        }
+    }
+}
